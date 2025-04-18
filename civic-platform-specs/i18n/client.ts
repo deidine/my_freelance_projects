b2e08a3bfb8a163e5074bbe5e8cb1f78cdeee59c
deidine <deidinemycours@gmial.com>
2025-04-18 09:45:02 +0000

    deidine

--- civic-platform-specs/i18n/client.ts
+++
@@ -1,8 +0,0 @@
-"use client"
-
-import { createI18nClient } from "next-international/client"
-
-export const { useI18n, useScopedI18n, I18nProviderClient, useChangeLocale, useCurrentLocale } = createI18nClient({
-  fr: () => import("./dictionaries/fr.json"),
-  ar: () => import("./dictionaries/ar.json"),
-})
--- civic-platform-specs/i18n/server.ts
+++
@@ -1,6 +0,0 @@
-import { createI18nServer } from "next-international/server"
-
-export const { getI18n, getScopedI18n, getStaticParams, getCurrentLocale } = createI18nServer({
-  fr: () => import("./dictionaries/fr.json"),
-  ar: () => import("./dictionaries/ar.json"),
-})
